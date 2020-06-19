a4e4bc0a2bfc8397b63d4339d3027840919ff4da
CharlRomero <romeokarlos12@gmail.com>
2020-06-19 01:10:10 -0500

    ready

--- BurbleRecursive/BurbleRecursive/BurbleRecursive.cpp
+++ BurbleRecursive/BurbleRecursive/BurbleRecursive.cpp
@@ -2,11 +2,24 @@
 //
 
 #include <iostream>
+#include "Sort.h"
 
-//int main()
-//{
-//    std::cout << "Hello World!\n";
-//}
+int main()
+{
+	int* arr = new int[7];
+	*arr = 5;
+	*(arr + 1) = 8;
+	*(arr + 2) = 4;
+	*(arr + 3) = 3;
+	*(arr + 4) = 4;
+	*(arr + 5) = 6;
+	*(arr + 6) = 2;
+	Sort<int> sort;
+	int size = 7;
+	sort.bubbleSort(arr, size);
+	for (size_t i = 0; i < 7; i++)
+		std::cout << *(arr + i) << std::endl;
+}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
