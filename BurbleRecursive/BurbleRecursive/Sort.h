5647699fd0a587519aa33353b5be2b2ddc819158
Abigail Cabascango <51092317+sacabascango1@users.noreply.github.com>
2020-06-19 10:23:51 -0500

    Document

--- BurbleRecursive/BurbleRecursive/Sort.h
+++ BurbleRecursive/BurbleRecursive/Sort.h
@@ -1,4 +1,18 @@
 #pragma once
+/******************************************************************
+*            UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE              *
+* CARRERA: Ingenieria de Software                                 *
+* SEMESTRE: Tercer semestre                                       *
+* MATERIA: Estructura de Datos                                    *
+* NRC: 6396                                                       *
+******************************************************************/
+
+/***********************************************************************
+ * Module:  IngresoDatos.h
+ * Author:  Carlos Romero, Abigail Cabascango
+ * Modified: viernes, 12 de junio de 2020 17:51:04
+ * Purpose: Ingresar datos por consola
+ **********************************************************************/
 #if !defined(__Sort_h)
 #define __Sort_h
 template <class T> class Sort {
@@ -8,9 +22,19 @@ public:
 private:
 	T temp;	
 };
-
+////////////////////////////////////////////////////////////////////////
+// Name:      template<class T> Pareja& Pareja::operator + (T p)
+// Purpose:    Constructor of Sort<T>::Sort() { this->temp = NULL; }
+// Parameters: 
+// Return:     
+////////////////////////////////////////////////////////////////////////
 template <class T> Sort<T>::Sort() { this->temp = NULL; }
-
+////////////////////////////////////////////////////////////////////////
+// Name:      template<class T> Pareja& Pareja::operator + (T p)
+// Purpose:    Implemet of Sort<T>::bubbleSort(T* arr, int size)
+// Parameters: T* arr, int size
+// Return:     void
+////////////////////////////////////////////////////////////////////////
 template <typename T> void Sort<T>::bubbleSort(T* arr, int size) {
 
 	if (size == 1)
