ce412430f45f3715080ae29f890a1ecdbf7fd959
CharlRomero <romeokarlos12@gmail.com>
2020-06-19 01:09:42 -0500

    ready

--- BurbleRecursive/BurbleRecursive/Sort.h
+++ BurbleRecursive/BurbleRecursive/Sort.h
@@ -1,6 +1,7 @@
 #pragma once
-template <class T>
-class Sort {
+#if !defined(__Sort_h)
+#define __Sort_h
+template <class T> class Sort {
 public:
 	void bubbleSort(T*,int);	
 	Sort();
@@ -8,3 +9,21 @@ private:
 	T temp;	
 };
 
+template <class T> Sort<T>::Sort() { this->temp = NULL; }
+
+template <typename T> void Sort<T>::bubbleSort(T* arr, int size) {
+
+	if (size == 1)
+		return;
+
+	for (size_t i = 0; i < size - 1; i++) {
+		if (*(arr + i) > * (arr + (i + 1))) {
+			temp = *(arr + i);
+			*(arr + i) = *(arr + (i + 1));
+			*(arr + (i + 1)) = temp;
+		}
+	}
+
+	bubbleSort(arr, size - 1);
+}
+#endif
